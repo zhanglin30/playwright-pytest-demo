b5938f32deebffdd8259f15922bf0499cf306879
张 <>
2024-04-24 10:29:04 +0800

    美化报告，增加titlie

--- TestCase/Test_home/Test_home.py
+++ TestCase/Test_home/Test_home.py
@@ -2,7 +2,7 @@
 # @Time : 2024/3/20 14:35
 # @Author : zl
 # @File : Test_home.py.py
-
+import allure
 
 from pages.HomePage.HomePage import HomePage
 from Tools.ReadYaml import yamlUtil
@@ -16,6 +16,7 @@ class Test_home_search:
     @pytest.mark.run(order=1)
     @pytest.mark.parametrize("args", yamlUtil(os.getcwd() + "/TestDatas/TestHomeData.yaml").read_yaml())
     def test_home_search(self, page, args):
+        allure.dynamic.title('搜索：'+args["value"])
         home_page = HomePage(page)
         home_page.navigate()
 
--- TestCase/Test_login/Test_login.py
+++ TestCase/Test_login/Test_login.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/3/20 14:35
+# @Author : zl
+# @File : Test_home.py.py
 from pages.LoginPage.LoginPage import LoginPage
 import pytest
 import allure
@@ -6,7 +10,8 @@ import allure
 
 class Test_login:
     @pytest.mark.run(order=0)
-
+    @allure.title('登录')
+    @allure.description('正确的账户密码登录')
     def test_login(self, page):
         login_page = LoginPage(page)
         login_page.navigate()
